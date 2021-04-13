b51320fb9a782d72ffe470f7e759553b7d067162
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-04-13 11:45:04 -0700

    Updated gitignore

--- MobTestApp/MobTestApp.Android/obj/Debug/90/android/environment.x86_64.s
+++ MobTestApp/MobTestApp.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"eae746d7-8cb3-4ffb-b432-4cb7de97958d"
+	.asciz	"a6d7782b-4ad7-4a59-b171-a766204197d8"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
