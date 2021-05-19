7dbb0ab738e8dc2c27eb113042f1b9bdf4b3e031
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-05-19 12:11:34 -0700

    Revert "Removed the frame from Homepage and Changed DailyDose source code for video"
    This reverts commit ab2fd839142241b43f8bf06379fc2286c508716f.

--- MobTestApp/MobTestApp/Views/DailyDosePage.xaml.cs
+++ MobTestApp/MobTestApp/Views/DailyDosePage.xaml.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.CommunityToolkit.UI.Views;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using YoutubeExplode;
-using YoutubeExplode.Videos.Streams;
 
 namespace MobTestApp.Views
 {
@@ -17,27 +15,6 @@ namespace MobTestApp.Views
         public DailyDosePage()
         {
             InitializeComponent();
-            GetVideoContent();
-        }
-
-        private async void GetVideoContent()
-        {
-            var youtube = new YoutubeClient();
-            string videoId = "ECxYJcnvyMw";
-            var videoURL = $"https://www.youtube.com/watch?v={videoId}";
-            var video = await youtube.Videos.GetAsync(videoURL);
-
-            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoURL);
-            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
-
-            if (streamInfo != null)
-            {
-                // Get the actual stream
-                // var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
-
-                // Then use it with MediaElement
-                mediaElement.Source = streamInfo.Url;
-            }
         }
     }
 }
