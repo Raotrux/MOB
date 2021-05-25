8d247d9e4d47d88edbd9050f280d91dd9b802145
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-05-25 14:21:14 -0700

    Adjusting colors in App and changing sources to determine color based on theme

--- MobTestApp/MobTestApp/Models/Item.cs
+++ MobTestApp/MobTestApp/Models/Item.cs
@@ -7,7 +7,6 @@ namespace MobTestApp.Models
         public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
         public string Thumbnail { get; set; }
     }
 }
