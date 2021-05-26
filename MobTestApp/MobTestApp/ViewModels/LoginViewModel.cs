d9968d7b5fa885e9dacfa79599979afb57ec4805
B. Foster <brandan@getfoster.net>
2021-05-25 22:48:16 -0700

    Adding WebRedirect Activity and View

--- MobTestApp/MobTestApp/ViewModels/LoginViewModel.cs
+++ MobTestApp/MobTestApp/ViewModels/LoginViewModel.cs
@@ -10,15 +10,23 @@ namespace MobTestApp.ViewModels
     {
         public Command LoginCommand { get; }
 
+        public Command ClickInit { get; }
+
         public LoginViewModel()
         {
-            LoginCommand = new Command(OnLoginClicked);     //insert the beginning of the OAuth flow here.
+            LoginCommand = new Command(OnLoginClicked);
+            ClickInit = new Command(InitOAuth);  //insert the beginning of the OAuth flow here.
         }
 
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        }
+
+        private async void InitOAuth(object obj)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(OAuthRedirect)}");
         }
     }
 }
