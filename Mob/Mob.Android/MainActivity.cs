a00b31519fa71129fd5327e4cc93218c0434b298
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-05-25 23:50:26 -0700

    Changing app icon on devices

--- Mob/Mob.Android/MainActivity.cs
+++ Mob/Mob.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android;
 
 namespace Mob.Droid
 {
-    [Activity(Label = "Mob", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Mо̄b", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
