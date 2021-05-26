e5936e56beb60de134dcba903214bf8a0655a76c
Raotrux <56573919+Raotrux@users.noreply.github.com>
2021-05-26 09:38:38 -0700

    Start of splash screen

--- Mob/Mob.Android/MainActivity.cs
+++ Mob/Mob.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android;
 namespace Mob.Droid
 {
     // This label changes the name of the app on the phone.
-    [Activity(Label = "Mо̄b", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Mо̄b", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
